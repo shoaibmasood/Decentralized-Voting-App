a0293bc7416851fae1fcb6f873156b821bc4dab8
Shoaib <sm22abr@herts.ac.uk>
2024-07-18 19:21:47 +0100

    Updated Landing Page text

--- app/LandingPage.js
+++ app/LandingPage.js
@@ -9,15 +9,13 @@ export default function Home() {
         <div className="container bg-slate-100 rounded-2xl mx-auto flex px-10 py-24  md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Decentralized Voting For a
+              Decentralized Voting App
               <br className="hidden lg:inline-block" />
-              Trustworthy Democracy
+              Using BlockChain
             </h1>
             <p className="mb-8 leading-relaxed">
-              Welcome to our plaform where cutting-edge technology meets
-              democratic participation.
-              <br />
               Explore a new era of secure, transparent and tamper-proof voting.
+              <br />
             </p>
             <div className="flex justify-center">
               <ConnectButton />
